8c6c177ffd6bd5ee0180ab2e1fb12c791b9ca4ac
Strapple <bysfz767@gmail.com>
2026-03-12 14:58:11 +0700

    FSM 2D

---
+++ AI-2D/Assets/SCRIPT FSM/FSM.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FSM : MonoBehaviour
+{
+    public enum State
+    {
+        Idle,
+        Chase,
+        Attack
+    }
+
+    public State currentState;
+    public Transform player;
+    public float chaseDistance = 2f;
+    public float speed = 2f;
+    public float attackDistance = 2f;
+
+
+    void Update()
+    {
+        float distance = Vector2.Distance(transform.position, player.position);
+
+        switch (currentState)
+        {
+            case State.Idle:
+                if (distance < chaseDistance)
+                {
+                    currentState = State.Chase;
+                }
+                break;
+
+            case State.Chase:
+                transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
+                if (distance < attackDistance)
+                {
+                    currentState = State.Attack;
+                }
+                break;
+
+            case State.Attack:
+                Debug.Log("Menyerang");
+                if (distance > attackDistance)
+                {
+                    currentState = State.Chase;
+                }
+                break;
+        }
+    }
+}
